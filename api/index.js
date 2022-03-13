8bdf86d7034b7c1e0ffce2dfa265b84c7e8f2079
Anwesha Mishra <anwesharoses@gmail.com>
2022-03-13 14:42:53 +0530

    .gitignore is now working

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const mongoose=require('mongoose')
 const dotenv=require ("dotenv")
 
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGO_URL,{
 app.use(express.json());
 
 app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
 
 app.listen(8800,()=>{
     console.log('Backend is running');
